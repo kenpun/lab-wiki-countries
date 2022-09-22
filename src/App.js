9fc18f56c713cde15a549964037d7f784cd63ccb
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-22 17:51:38 +0200

    create countriesList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import CountriesList from './components/CountriesList'
+import countriesData from './countries.json'
 import "./App.css";
 
 function App() {
   return <div className="App">
     <Navbar />
-    <CountriesList />
+    <CountriesList path="/countries" element={<CountriesList />} countries={countriesData}/>
+    
   </div>;
 }
 export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import countriesData from "../countries.json"
 
 function CountriesList() {
-    return
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        setCountries(countriesData);
+    }, [])
+
+    return (
+        <div>
+            <h2>Countries</h2>
+            {countries.map((country) => {
+                console.log(country)
+                return (
+                    <div key={country._id} className="country">
+                        <Link to={`/countryDetails/${country.alpha3Code}`}><h3>{country.name.common}</h3></Link>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default CountriesList;
